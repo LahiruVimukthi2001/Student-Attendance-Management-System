e70954498f4bf29cf2ccb29780d296f8367b4056
Lahiru Vimukthi <lahiruvimukthi82@gmail.com>
2026-02-08 21:03:34 +0530

    Update index.php

--- index.php
+++ index.php
@@ -84,3 +84,65 @@ include('header.php');
 </div>
 
 
+<script>
+$(document).ready(function(){
+	 
+  var dataTable = $('#student_table').DataTable({
+    "processing":true,
+    "serverSide":true,
+    "order":[],
+    "ajax":{
+      url:"attendance_action.php",
+      type:"POST",
+      data:{action:'index_fetch'}
+    }
+  });
+
+  $('.input-daterange').datepicker({
+    todayBtn:"linked",
+    format:"yyyy-mm-dd",
+    autoclose:true,
+    container: '#formModal modal-body'
+  });
+
+  $(document).on('click', '.report_button', function(){
+    var student_id = $(this).attr('id');
+    $('#student_id').val(student_id);
+    $('#formModal').modal('show');
+  });
+
+  $('#create_report').click(function(){
+    var student_id = $('#student_id').val();
+    var from_date = $('#from_date').val();
+    var to_date = $('#to_date').val();
+    var error = 0;
+    if(from_date == '')
+    {
+      $('#error_from_date').text('From Date is Required');
+      error++;
+    }
+    else
+    {
+      $('#error_from_date').text('');
+    }
+    if(to_date == '')
+    {
+      $('#error_to_date').text('To Date is Required');
+      error++;
+    }
+    else
+    {
+      $('#error_to_date').text('');
+    }
+
+    if(error == 0)
+    {
+      $('#from_date').val('');
+      $('#to_date').val('');
+      $('#formModal').modal('hide');
+      window.open("report.php?action=student_report&student_id="+student_id+"&from_date="+from_date+"&to_date="+to_date);
+    }
+  });
+
+});
+</script>
